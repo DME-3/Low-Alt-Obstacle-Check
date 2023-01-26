a7a6752f83746b7a07a10ebe67c415b1009c5a0e
Dimitri <dimitri@dme3.eu>
2023-01-26 13:42:59 +0100

    finished step 3

--- 3_remove_bad_NAC.py
+++ 3_remove_bad_NAC.py
@@ -96,6 +96,10 @@ if __name__ == "__main__":
 
     df_path = arg1
 
+    if os.path.isfile(df_path + '/nac_df.json'):
+        print('Error: nac_df.json already present in %s. Check if the dataframes have not been processed yet.'%(df_path))
+        sys.exit(1)
+
     print('Loading dataframes...')
 
     # Load clean_inf_df
@@ -116,18 +120,35 @@ if __name__ == "__main__":
     nac_df = fetch_data_from_OSN(gdf)
     ac_bad_nac = nac_df[nac_df['positionnac'] < NAC_MIN].icao24.unique()
     print('Data retrieved. Out of %s flights, %s flights have a NACp below %s'%(str(len(nac_df)), str(len(ac_bad_nac)), str(NAC_MIN)))
+    print('Saving the dataframes...')
 
     #save the nac_df
-    # ...
+    nac_json = df_path + '/nac_df.json'
+    nac_df.to_json(nac_json)
+    print('Saved %s'%(nac_json))
 
-    index_to_drop = clean_inf_df[clean_inf_df['ICAO'].isin(ac_bad_nac)].index
+    index_to_drop = clean_inf_df[clean_inf_df['icao24'].isin(ac_bad_nac)].index
     clean_inf_df.drop(index_to_drop, inplace = True)
 
-    gnd_index_to_drop = clean_gnd_inf_df[clean_gnd_inf_df['ICAO'].isin(ac_bad_nac)].index
+    gnd_index_to_drop = clean_gnd_inf_df[clean_gnd_inf_df['icao24'].isin(ac_bad_nac)].index
     clean_gnd_inf_df.drop(gnd_index_to_drop, inplace = True)
 
-    # drop bad nac from gdf
-    # ...
+    gdf_index_to_drop = gdf[gdf['icao24'].isin(ac_bad_nac)].index
+    gdf.drop(gdf_index_to_drop, inplace = True)
 
-    # save the updated dataframes
-    # ...
+    for gdf_file in gdf_files:
+        gdf_json = gdf_file
+        gdf.to_json(gdf_json)
+        print('Saved %s'%(gdf_json))
+    
+    for inf_file in inf_df_files:
+        inf_json = inf_file
+        clean_inf_df.to_json(inf_json)
+        print('Saved %s'%(inf_json))
+
+    for gnd_inf_file in gnd_inf_df_files:
+        gnd_inf_json = gnd_inf_file
+        clean_gnd_inf_df.to_json(gnd_inf_json)
+        print('Saved %s'%(gnd_inf_json))
+
+    print('Done, exiting.')
